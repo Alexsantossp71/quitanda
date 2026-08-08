d66d5a079e72507cd2e5e6734154c376200569dd
Alexandre Ramos <78726166+Alexsantossp71@users.noreply.github.com>
2026-08-08 13:26:20 -0300

    feat: integra DemoOrdersRepository quando kDemoMode=true

--- lib/src/pages/orders/view/components/repository/orders_repository.dart
+++ lib/src/pages/orders/view/components/repository/orders_repository.dart
@@ -1,15 +1,21 @@
+import 'package:greengrocery/src/config/demo_mode.dart';
 import 'package:greengrocery/src/constants/endpoints.dart';
 import 'package:greengrocery/src/models/order_model.dart';
 import 'package:greengrocery/src/pages/auth/controller/auth_controller.dart';
 import 'package:greengrocery/src/pages/orders/orders_result/orders_result.dart';
 import 'package:greengrocery/src/services/http_manager.dart';
+import 'package:greengrocery/src/pages/orders/view/components/repository/demo_orders_repository.dart';
 
 class OrderRepository {
   final _httpManager = HttpManager();
-  final authController = AuthController();
+  final DemoOrdersRepository _demoRepo = DemoOrdersRepository();
 
   Future<OrdersResult<List<OrderModel>>> getAllOrders(
       {required String userId, required String token}) async {
+    if (kDemoMode) {
+      return _demoRepo.getAllOrders(userId: userId, token: token);
+    }
+
     final result = await _httpManager.restRequest(
       url: EndPoints.getAllOrders,
       method: HttpMethodos.post,
@@ -17,8 +23,7 @@ class OrderRepository {
         'user': userId,
       },
       headers: {
-        'X-Parse-Session-Token': token
-        //authController.user.token
+        'X-Parse-Session-Token': token,
       },
     );
 
